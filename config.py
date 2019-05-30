2f2b30ca809966e59eba5759221e2fb69b11067e
EC2 Default User <ec2-user@ip-172-16-90-38.eu-west-2.compute.internal>
2019-05-30 16:08:08 +0000

    wip

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from bunch import Bunch
 
 
 config = {
-    "SEQ_LEN": 100,
-    "MAX_EXAMPLE_LEN": 100,
+    "SEQ_LEN": 4,
+    "MAX_EXAMPLE_LEN": 512,
     "BATCH_SIZE": 2,
     "BUFFER_SIZE": 1,
     "VOCAB_LEN": 30522
