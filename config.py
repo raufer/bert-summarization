e7a1fc1ae037cc9633c97fd53e3b225a0f0072c1
EC2 Default User <ec2-user@ip-172-16-79-198.eu-west-2.compute.internal>
2019-06-08 18:00:42 +0000

    wup

--- config.py
+++ config.py
@@ -4,12 +4,14 @@ from bunch import Bunch
 config = {
     "SEQ_LEN": 3,
     "MAX_EXAMPLE_LEN": 3,
+    'NUM_EPOCHS': 1,    
     "BATCH_SIZE": 2,
     "BUFFER_SIZE": 1,
     "VOCAB_LEN": 30522,
     "INITIAL_LR": 0.003,
     "WARMUP_STEPS": 4000,
-    "LOGDIR": 'log'
+    "LOGDIR": 'log',
+    "CHECKPOINTDIR": 'checkpoint'
 }
 
 config = Bunch(config)
